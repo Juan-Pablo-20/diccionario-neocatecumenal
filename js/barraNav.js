cee3af6657aa07891153777d50fbb0cedce7daa9
Juan Pablo <juan-ballesteros-26@hotmail.com>
2021-05-03 00:37:47 -0500

    se inicio el diseño de la pagina para las palabras

--- js/barraNav.js
+++ js/barraNav.js
@@ -9,12 +9,15 @@ $(document).ready(function () {
         if (barra.clientWidth > 725) {
             if (window.scrollY > 2) {
                 barra.style.backgroundColor = "rgba(165, 42, 42, 0.700)";
-                barra.style.position = "fixed";
                 barra.style.top = "0";
             } else {
                 barra.style.backgroundColor = "brown";
-                barra.style.position = "relative";
             }
         }
     })
+
+    const boton = document.querySelector(".imprimir").addEventListener("click", () => {
+        print();
+    });
 });
+
