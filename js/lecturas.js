da4944f025d0ec408c9a25bcd181afd44882e9e7
Juan Pablo <juan-ballesteros-26@hotmail.com>
2021-05-03 20:07:47 -0500

    se programo las votaciones y se pusieron las imagenes que le faltaban al index

--- js/lecturas.js
+++ js/lecturas.js
@@ -1,9 +1,9 @@
-const lect = document.querySelector(".lectura");
-const lecturas = document.getElementsByClassName("lectura");
+const lect = document.querySelector(".lectura"); //se selecciona el elemento con la clase .lectura
+const lecturas = document.getElementsByClassName("lectura"); //se selecciona la lista de elementos con la clase .lectura
 
 for (lectura in lecturas) {
     if (lectura <= lecturas.length) {
-        lecturas[lectura].classList.add(`le${lectura}`);
+        lecturas[lectura].classList.add(`le${lectura}`); //a cada elemento con dicha clase se le añade otra (le1)
     }
 }
 
@@ -11,9 +11,10 @@ const cuadro = document.querySelector(".words");
 
 var votos = 0;
 var existe = false;
-for (let i = 0; i < lecturas.length; i++) {
+for (let i = 0; i < lecturas.length; i++) { //recorre la lista de elementos con la clase .lectura
     const lct = document.querySelector(`.le${i}`);
-    lct.addEventListener("click", () => {
+    lct.addEventListener("click", () => {//a cada uno se le asigna un evento
+        lct.style.fontWeight = "bold";
         cuadro.innerHTML += `<div class="botones">
         <div class="lectBotn">
             <span>${lct.innerHTML}</span>
@@ -21,30 +22,40 @@ for (let i = 0; i < lecturas.length; i++) {
             <div class="rojo rojo${i}"><i class="fas fa-arrow-down" style="color: red;"></i></div>
         </div>
         <div class="conteo">
-            <span class="conteo${i}">0 votos</span>
+            <span class="conteo${i} cverde${i} crojo${i}">0</span>
         </div>
     </div>`;
-    existe = true;
-    asignarVotos(existe);
-    })
-
+        escogidas.push(i);//se añade el numero i al array escogidas
+        existe = true;
+        asignarVotos(existe);
+    });
 }
+
+const escogidas = [];
 
 const botones = document.getElementsByClassName("botones");
 
-const asignarVotos = (exist) => { //REVISAR Y CORREGIR
-    if(exist){
-        for(let i = 0; i < botones.length - 1; i++){
-            console.log(i)
-            document.querySelector(`.verde${i}`).addEventListener("click", () => {
-                votos++;
-                document.querySelector(`conteo${i}`).innerHTML = `${votos} votos`;
+const asignarVotos = (exist) => {
+    if (exist) {
+        var numero;
+        var conteo = 0;
+        for (escogida of escogidas) { //se recorre el array escogidas que contiene los numeros de las lecturas seleccionadas
+            const verde = document.querySelector(`.verde${escogida}`);
+            verde.addEventListener("click", () => { //se le asigna un evento a cada boton verde
+                numero = verde.classList[verde.classList.length - 1];
+                const voto = document.querySelector(`.c${numero}`); //se busca el cuadro de votos con esa clase cverde1
+                conteo = parseInt(voto.innerHTML);
+                conteo++;
+                voto.innerHTML = conteo;
             });
-        
-            document.querySelector(`.rojo${i}`).addEventListener("click", () => {
-                if (votos >= 0) {
-                    votos--;
-                    document.querySelector(`conteo${i}`).textContent = `${votos} votos`;
+            const rojo = document.querySelector(`.rojo${escogida}`); //lo mismo pero para botones rojos
+            rojo.addEventListener("click", () => {
+                numero = rojo.classList[rojo.classList.length - 1];
+                const voto = document.querySelector(`.c${numero}`);
+                conteo = parseInt(voto.innerHTML);
+                if (conteo > 0) {
+                    conteo--;
+                    voto.innerHTML = conteo;
                 }
             });
         }
